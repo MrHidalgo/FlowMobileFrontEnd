1fa48ebfab83bcae2faa154267c89cb68cdb9446
MrHidalgo <Luckyhidalgo@gmail.com>
2018-01-30 16:47:55 +0200

    Testing deploy v006

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ const http = require('http');
 http.createServer(function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end('Hello Ionic! To beat shit out someone');
 
 }).listen(process.env.PORT || 8080);
